67c3d6a2687c3353afb5a91505c94e38be080b1b
aryan.singh <aryan.singh@thoughtworks.com>
2021-02-27 19:02:03 +0530

    RESOLVED LINTING ERROR IN WEBPACK.CONFIG.JS FILE

--- webpack.config.js
+++ webpack.config.js
@@ -4,52 +4,52 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const cones = require('./cones.json');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index'],
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CopyWebpackPlugin([{ from: 'public/index.html' }])
-  ],
-  devServer: {
-    port: 3000,
-    contentBase: path.join(__dirname, './public'),
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-    before: function(app) {
-      app.get('/api/cones', function(req, res) {
-        res.json(cones);
-      });
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-	  },
-	  {
-		test: /\.css$/,
-		use: ["style-loader", "css-loader"]		,
-	  },
-	  {
-		test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg|jpg)(\?[a-z0-9=.]+)?$/,
-		loader: "url-loader"
-		},		
-    ]
-  },
-  resolve: {
-    alias: {
-        // "@ag-grid-community/core/modules": path.resolve('./node_modules/@ag-grid-community/core/dist/es2015/modules'),
-        "@ag-grid-community/core": path.resolve('./node_modules/@ag-grid-community/core'),
-        // "ag-grid-enterprise": path.resolve('./node_modules/ag-grid-enterprise'),
-        react: path.resolve('./node_modules/react')
+    entry: ['babel-polyfill', './src/index'],
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
     },
-    extensions: ['.js', '.jsx']
-},
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([{ from: 'public/index.html' }])
+    ],
+    devServer: {
+        port: 3000,
+        contentBase: path.join(__dirname, './public'),
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        before: function(app) {
+            app.get('/api/cones', function(req, res) {
+                res.json(cones);
+            });
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+	  },
+	  {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+	  },
+	  {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg|jpg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader'
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+        // '@ag-grid-community/core/modules': path.resolve('./node_modules/@ag-grid-community/core/dist/es2015/modules'),
+            '@ag-grid-community/core': path.resolve('./node_modules/@ag-grid-community/core'),
+            // 'ag-grid-enterprise': path.resolve('./node_modules/ag-grid-enterprise'),
+            'react': path.resolve('./node_modules/react')
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
 };
